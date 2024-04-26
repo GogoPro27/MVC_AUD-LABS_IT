d63ccc810ad486e3d2d8996f2936b97272579205
Gorazd Windows <gorazdfilipovski@gmail.com>
2024-04-26 07:49:27 -0700

    aud 2 - MVC

--- MVC_vezbi/Controllers/MoviesController.cs
+++ MVC_vezbi/Controllers/MoviesController.cs
@@ -10,27 +10,95 @@ namespace MVC_vezbi.Controllers
     public class MoviesController : Controller
     {
         //ova kje ni bide MODELOT vvvvvv
-        public static Movie movie = new Movie() { Name="Shrek", Rating=5,DownloadUrl="#",ImageUrl= @"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS92cMXfUwQSMFMcS-U9JrsKK5XNJMw-P-Sus1MuxmWVHVJ03AC-DtMc-betZYjA6UaD7O-fmJ6MrXj4urZStnTHZr_r6q7BMC4hYDA6IE" };
-        public static List<Client> clients = new List<Client>() {
-            new Client(){ Name="Aleksandar S.",MovieCard="1234",Address="bul. Partizanski odredi 19",Phone="070390346",Age=17},
-            new Client(){ Name="Gorazd F.",MovieCard="321",Address="Vladimir Komarov",Phone="070333000",Age=20},
-            new Client(){ Name="Trajko T.",MovieCard="2345",Address="Ilindenska 15",Phone="072233111",Age=29}
-        }; //ova kje ni bide MODELOT ^^^^^
+        public static List<Movie> moviesList = new List<Movie>() { 
+            new Movie() { Name = "Shrek", Rating = 5, DownloadUrl = "#", ImageUrl = @"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS92cMXfUwQSMFMcS-U9JrsKK5XNJMw-P-Sus1MuxmWVHVJ03AC-DtMc-betZYjA6UaD7O-fmJ6MrXj4urZStnTHZr_r6q7BMC4hYDA6IE" } 
+        };
+        public static List<Client> clients = new List<Client>() {}; //ova kje ni bide MODELOT ^^^^^
         // GET: Movies
         public ActionResult Index()
         {
             return View();
         }
-        public ActionResult Random()
+        public ActionResult ShowMovie(int id)
         {
             MovieRentals model = new MovieRentals();
             model.clients = clients;
-            model.movie = movie;
+            model.movie = moviesList.ElementAt(id);
             return View(model);
+        }
+        public ActionResult GetAllMovies()
+        {
+            return View(moviesList);
         }
         public ActionResult ShowClient(int id) { 
        
             return View(clients.ElementAt(id));
+        }
+     
+        public ActionResult NewMovie()
+        {
+            Movie model = new Movie();
+            return View(model);
+        }
+        public ActionResult NewClient()
+        {
+            Client model = new Client();
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult InsertNewClient(Client model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewClient", model);
+            }
+            clients.Add(model);
+            return View("GetAllMovies",moviesList);
+        }
+
+
+        [HttpPost]//zasto ova ?
+        public ActionResult InsertNewMovie(Movie model) {
+            if (!ModelState.IsValid)
+            {
+                return View("NewMovie", model);
+                //ova e mn jako i BITNO!!!
+            }
+            moviesList.Add(model);
+            return View("GetAllMovies",moviesList);
+        }
+        public ActionResult EditMovie(int id)
+        {
+            var model = moviesList.ElementAt(id);
+            //pametno za da posle go znam redniot broj
+            model.Id= id;
+            return View(model);
+        }
+        [HttpPost]
+        public ActionResult EditMovieInList(Movie model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewMovie", model);
+                //ova e mn jako i BITNO!!!
+            }
+
+            var forUpdate = moviesList.ElementAt(model.Id);
+            forUpdate.Name = model.Name;
+            forUpdate.ImageUrl = model.ImageUrl;
+            forUpdate.DownloadUrl = model.DownloadUrl;
+            forUpdate.Rating = model.Rating;
+
+            return View("GetAllMovies", moviesList);
+        }
+
+        public ActionResult DeleteMovie(int id) {
+            if (moviesList.Count > 0)
+            {
+                moviesList.RemoveAt(id);
+            }
+            
+            return View("GetAllMovies", moviesList);
         }
     }
 }
